d0de4221c5aee1a92acd568e8f05297088d3d2b7
zensuyeom <zensuyeom@gmail.com>
2020-11-09 15:34:35 +0900

    dbconne.php 파일 수정

--- lib/dbconn.php
+++ lib/dbconn.php
@@ -1,5 +1,5 @@
 <?
-    $connect=mysql_connect( "localhost", "zensuyeom", "dhfpdh55") or  
+    $connect=mysql_connect( "localhost", "", "") or  
         die( "SQL server에 연결할 수 없습니다."); 
 
     mysql_select_db("zensuyeom",$connect);
